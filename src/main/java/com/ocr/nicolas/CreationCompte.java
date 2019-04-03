15634940d75f1c3a0505ea2d49ab0efca3bc44d2
Papoune <npapon@live.fr>
2019-04-03 21:03:46 +0200

    Prod : commit 3 03042019

--- src/main/java/com/ocr/nicolas/CreationCompte.java
+++ src/main/java/com/ocr/nicolas/CreationCompte.java
@@ -6,11 +6,16 @@ public class CreationCompte {
 
     private String login;
     private String motDePasse;
+    private String nom;
+    private String prenom;
     private Affaire affaireFavorite;
+
 
     public CreationCompte(String login,String motDePasse,Affaire affaireFavorite)
     {this.login =login;
     this.motDePasse=motDePasse;
+    this.prenom=prenom;
+    this.nom = nom;
     this.affaireFavorite=affaireFavorite;}
 
 
--- src/main/java/web/Serenity.java
+++ src/main/java/web/Serenity.java
@@ -1,6 +1,7 @@
 package web;
 
 import com.ocr.nicolas.Affaire;
+import com.ocr.nicolas.CreationCompte;
 import com.ocr.nicolas.Programme;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,6 +32,8 @@ resp.setContentType("text/html");
 
     @Override
     public void init() throws ServletException {
+
+
         Affaire StarWars = new Affaire(3,"StarWars","StarWars un nouvel Espoir",
                 new HashMap<String, Integer>(),true,new ArrayList<String>(),new ArrayList<Programme>());
     StarWars.addCaracteristiqueAffaire("Placement de produit");
@@ -40,13 +44,38 @@ resp.setContentType("text/html");
 
     Affaire Braquo = new Affaire(4,"Braquo",null,new HashMap<String, Integer>(),
             true,new ArrayList<>(),new ArrayList<>());
+    Programme BraquoProgramme1 = new Programme(Braquo,1,"Max",null);
+
+        Programme BraquoProgramme2 = new Programme(Braquo,2,"Emy",null);
+        Braquo.addProgrammesAffaire(BraquoProgramme1);
+        Braquo.addProgrammesAffaire(BraquoProgramme2);
+
+        affaires.add(StarWars);
+        affaires.add(Braquo);
     }
 
 
 
 
     private void afficherFormulaire(HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Formulaire");
+        String form =
+                "<form action=\"Compte\">"
+                        +"<label for=\"nom\">Nom : </label><input type=\"text\" name=\"nom\" id=\"nom\"/>"
+                        +"<br/>"
+                        +"<label for=\"prenom\">Prenom : </label><input type=\"text\" name=\"prenom\" id=\"prenom\"/>"
+                        +"<br/>"
+                        +"<label for=\"login\">Login : </label><input type=\"text\" name=\"login\" id=\"login\"/>"
+                        +"<br/>"
+                        +"<label for=\"password\">Mot de passe : </label><input type=\"password\" name=\"password\" " +
+                        "id=\"password\" placeholder=\"code secret\"/>"
+                +"</form>";
+
+        resp.getWriter().println("<b>PAGE DE CREATION DE COMPTE</b>"
+                +"<br/><br/>"
+        + "CREER VOTRE COMPTE EN REMPLISSANT LE FORMULAIRE"
+                + form
+        );
+
     }
 
     private void afficherResultatFormulaire() {
